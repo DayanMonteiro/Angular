bdc9626eed9fe5888117f7929842d01ab0359e7c
Dayan Monteiro <dmsmamb@gmail.com>
2021-05-13 17:27:11 -0300

    logica

--- CRUD/frontend/src/app/components/product/product-read2/product-read2-datasource.ts
+++ CRUD/frontend/src/app/components/product/product-read2/product-read2-datasource.ts
@@ -7,7 +7,6 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 
 
 
-// TODO: replace this with real data from your application
 const EXAMPLE_DATA: Product[] = [
   {id: 1, name: 'Hydrogen'},
   {id: 2, name: 'Helium'},
@@ -31,11 +30,7 @@ const EXAMPLE_DATA: Product[] = [
   {id: 20, name: 'Calcium'},
 ];
 
-/**
- * Data source for the ProductRead2 view. This class should
- * encapsulate all logic for fetching and manipulating the displayed data
- * (including sorting, pagination, and filtering).
- */
+
 export class ProductRead2DataSource extends DataSource<Product> {
   data: Product[] = EXAMPLE_DATA;
   paginator: MatPaginator | undefined;
@@ -45,34 +40,22 @@ export class ProductRead2DataSource extends DataSource<Product> {
     super();
   }
 
-  /**
-   * Connect this data source to the table. The table will only update when
-   * the returned stream emits new items.
-   * @returns A stream of the items to be rendered.
-   */
+ 
   connect(): Observable<Product[]> {
-    if (this.paginator && this.sort) {
-      // Combine everything that affects the rendered data into one update
-      // stream for the data-table to consume.
-      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
-        .pipe(map(() => {
-          return this.getPagedData(this.getSortedData([...this.data ]));
-        }));
-    } else {
-      throw Error('Please set the paginator and sort on the data source before connecting.');
-    }
+    const dataMutations = [
+      observableOf(this.data),
+      this.paginator.page,
+      this.sort.sortChange
+    ];
+
+    return merge(...dataMutations).pipe(map(() => {
+      return this.getPagedData(this.getSortedData([...this.data]));
+    }));
   }
 
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
+
   disconnect(): void {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
   private getPagedData(data: Product[]): Product[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
@@ -82,10 +65,6 @@ export class ProductRead2DataSource extends DataSource<Product> {
     }
   }
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
   private getSortedData(data: Product[]): Product[] {
     if (!this.sort || !this.sort.active || this.sort.direction === '') {
       return data;
@@ -102,7 +81,6 @@ export class ProductRead2DataSource extends DataSource<Product> {
   }
 }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
 function compare(a: string | number, b: string | number, isAsc: boolean): number {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
