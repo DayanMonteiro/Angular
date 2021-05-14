0c1716a74eb0ac081b03cef2df7881313f2abe3c
DayanMonteiro <dmsmamb@gmail.com>
2021-05-14 00:11:15 -0300

    expandido a lógica

--- CRUD/frontend/src/app/app.module.ts
+++ CRUD/frontend/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePT from '@angular/common/locales/pt';
 import  { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePT);
 
@@ -47,6 +48,7 @@ registerLocaleData(localePT);
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
